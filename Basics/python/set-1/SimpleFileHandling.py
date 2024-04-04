1b2478abb9e1b322a6eb731d09c23639b221c52a
Jatin <chaurasiyajatin68@gmail.com>
2024-04-04 18:00:03 +0530

    jatin

--- Basics/python/set-1/SimpleFileHandling.py
+++ Basics/python/set-1/SimpleFileHandling.py
@@ -1,7 +1,7 @@
 # writing  to a file
-with open("output.txt","w") as file:
+with open("Basics/python/set-1/output.txt","w") as file:
     file.write("Hello, this is jatin.")
 
 #Reading from a file
-with open("output.txt","r") as file:
+with open("Basics/python/set-1/output.txt","r") as file:
     print("data from output.txt file is :",file.read())    
