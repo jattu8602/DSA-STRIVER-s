4e8a024733c0c8556124bacda4725cbbc1fd48cd
Jatin <chaurasiyajatin68@gmail.com>
2024-05-19 14:21:56 +0530

    jatin

--- dsa/pivot.cpp
+++ dsa/pivot.cpp
@@ -1,29 +1,30 @@
-#include<iostream>
-#include<stdio.h>
+#include <iostream>
+#include <stdio.h>
 using namespace std;
-int getPivot(int arr[],int n){
+int getPivot(int arr[], int n)
+{
     int s = 0;
-    int e = n-1;
-    int mid = s+(e-s)/2;
-    while(s<e)
+    int e = n - 1;
+    int mid = s + (e - s) / 2;
+    while (s < e)
     {
-        if(arr[mid]>=arr[0]){
-            s = mid+1;
+        if (arr[mid] >= arr[0])
+        {
+            s = mid + 1;
         }
-        else{
+        else
+        {
             e = mid;
         }
-        mid= s+(e-s)/2;
+        mid = s + (e - s) / 2;
     }
-    return e;//you can return both e and s;
-
-
+    return e; // you can return both e and s;
 }
 
-int main(){
-    int arr[5] = {2,3,45,56,3};
-    cout<<"pivot is:"<<getPivot(arr,5);
-
+int main()
+{
+    int arr[5] = {2, 3, 45, 56, 3};
+    cout << "pivot is:" << getPivot(arr, 5);
 
     return 0;
 }
